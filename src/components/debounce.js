d7254ac3eeb78785759774a60959f1e7d2246d69
sanaf1333 <101400000+sanaf1333@users.noreply.github.com>
2023-02-28 20:46:43 +0500

    Add files via upload

--- src/components/debounce.js
+++ src/components/debounce.js
@@ -1,5 +1,5 @@
-import { useCallback } from 'react';
-import { debounce } from 'lodash';
+import { useCallback } from "react";
+import { debounce } from "lodash";
 
 const useDebouncedCallback = (callback, delay) => {
   const debouncedCallback = useCallback(
